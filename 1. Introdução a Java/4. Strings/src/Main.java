3bb3df343954fb15b60df904c8b7577bd6bc95ef
pablo <dalbem@gmail.com>
2024-02-23 17:15:40 -0300

    Introdução à linguagem Java

--- 1. Introdução a Java/4. Strings/src/Main.java
+++ 1. Introdução a Java/4. Strings/src/Main.java
@@ -1,8 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        String nomeCurso = "Programação Orientadad a Objetos";
+        String nomeCurso = "Programação Orientada a Objetos";
         System.out.println("O nome do curso é: "+ nomeCurso);
+
+        for (char c : nomeCurso.toCharArray())
+            System.out.print(c); // imprime Programação Orientada a Objetos,  caractere por caractere
+
+        System.out.println("Char no index 4: " + nomeCurso.charAt(4)); // mostra 'r'
+        System.out.println("Index de 'o': " + nomeCurso.indexOf('o')); // mostra 2
+        System.out.println("Length da string: " + nomeCurso.length()); // mostra o comprimento da string
+        System.out.println(nomeCurso.toUpperCase()); // PROGRAMAÇÃO ORIENTADA A OBJETOS
+        System.out.println(nomeCurso.toLowerCase()); // programação orientada a objetos
+        System.out.println(nomeCurso.substring(0, 11)); // Programação - substring from indexes 0 to 1
 
         String a = "POO";
         String b = a;  //variável b recebe a, portanto b="POO"
@@ -16,11 +26,25 @@ public class Main {
         iguais = a.equals(d); //retorna true
 
 
+        String literalA = "Object-oriented";
+        String literalB = "Object-oriented";
+        String objectA = new String("Object-oriented");
+        String objectB = new String("Object-oriented");
+
+        System.out.println(literalA == literalB); // true, apontam para o mesmo objeto no pool de string
+        System.out.println(objectA == objectB);  // false, apontam para objetos diferentes no heap
+        System.out.println(literalA == objectA); // false, um objeto está no pool de string e outro no heap
+        System.out.println(literalA.equals(objectA)); // true, equals compara o conteúdo e não a referência à memória
+
+
         // Concatenar Strings
         String primeiroNome = "Maria ";
         String segundoNome = "Silva";
         String nomeCompleto = primeiroNome.concat(segundoNome);
+        //OU assim:
+        //String nomeCompleto = primeiroNome+" "+segundoNome
         System.out.println(nomeCompleto);
+
 
         //Tamanho da String
         String nomeFaculdade = "IFSP";
--- 1. Introdução a Java/6. ComandoSwitch/src/Main.java
+++ 1. Introdução a Java/6. ComandoSwitch/src/Main.java
@@ -90,5 +90,57 @@ public class Main {
         }
 
 
+        //Exemplos do SWITCH para o Java 21
+
+        LocalDate date = LocalDate.now();
+
+        switch (date.getDayOfWeek()) {
+            case MONDAY -> System.out.println("It is Monday");
+            case TUESDAY -> System.out.println("It is Tuesday");
+            case WEDNESDAY -> System.out.println("It is Wednesday");
+            case THURSDAY -> System.out.println("It is Thursday");
+            case FRIDAY -> System.out.println("It is Friday");
+            case SATURDAY -> System.out.println("It is Saturday");
+            case SUNDAY -> System.out.println("It is Sunday");
+        }
+
+        final String dayAsString = switch (date.getDayOfWeek()) {
+            case MONDAY -> "It is Monday";
+            case TUESDAY -> "It is Tuesday";
+            case WEDNESDAY -> "It is Wednesday";
+            case THURSDAY -> "It is Thursday";
+            case FRIDAY -> "It is Friday";
+            case SATURDAY -> "It is Saturday";
+            case SUNDAY -> "It is Sunday";
+        };
+        System.out.println(dayAsString);
+
+        int numLetters = 0;
+        switch (date.getDayOfWeek()) {
+            case MONDAY: case FRIDAY: case SUNDAY:
+                numLetters = 6;
+                break;
+            case TUESDAY:
+                numLetters = 7;
+                break;
+            case THURSDAY:
+                numLetters = 8;
+                break;
+            case WEDNESDAY:
+                numLetters = 9;
+                break;
+        }
+        System.out.println(numLetters);
+
+        System.out.println(switch (date.getDayOfWeek()) {
+            case MONDAY, FRIDAY, SUNDAY -> 6;
+            case TUESDAY -> 7;
+            case THURSDAY -> 8;
+            case WEDNESDAY -> 9;
+            default -> 0;
+        });
+
+
+
     }
 }
