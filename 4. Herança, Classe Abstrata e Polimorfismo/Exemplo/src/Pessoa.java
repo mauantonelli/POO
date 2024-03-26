975a4d687ebe4b1d325e8fb8e3635b950839f478
pablo <dalbem@gmail.com>
2024-03-26 15:12:31 -0300

    Herança, classe abstrata e polimorfismo

---
+++ 4. Herança, Classe Abstrata e Polimorfismo/Exemplo/src/Pessoa.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public abstract class Pessoa {
+    private String endereco;
+    private String telefone;
+    private String email;
+
+    //método abstrato. Classes filhas (subclasses) deverão implementá-lo
+    public abstract void mostrarDados();
+
+    public Pessoa(String endereco, String telefone, String email) {
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+    public Pessoa() {
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
